f914e4b4de2b6be69350af73a025886b096291a2
HelloKitty <andrew.blakely@ymail.com>
2020-12-29 02:00:10 -0600

    Added IRPGCharacterRepository

---
+++ src/Glader.ASP.RPGCharacter.Server/Database/Repository/DefaultRPGCharacterRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using Microsoft.EntityFrameworkCore;
+
+namespace Glader.ASP.RPGCharacter
+{
+	/// <summary>
+	/// Default EF Core database-backed implementation of <see cref="IRPGCharacterRepository"/>
+	/// </summary>
+	public sealed class DefaultRPGCharacterRepository : GeneralGenericCrudRepositoryProvider<int, DBRPGCharacter>, IRPGCharacterRepository
+	{
+		public DefaultRPGCharacterRepository(DbSet<DBRPGCharacter> modelSet, DbContext context) 
+			: base(modelSet, context)
+		{
+
+		}
+	}
+}
---
+++ src/Glader.ASP.RPGCharacter.Server/Database/Repository/IRPGCharacterRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace Glader.ASP.RPGCharacter
+{
+	/// <summary>
+	/// <see cref="IGenericRepositoryCrudable{TKey,TModel}"/> for <see cref="DBRPGCharacter"/>.
+	/// </summary>
+	public interface IRPGCharacterRepository : IGenericRepositoryCrudable<int, DBRPGCharacter>
+	{
+
+	}
+}
