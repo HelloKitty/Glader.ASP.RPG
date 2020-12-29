3879959e3e905b6af93e831d31fa57c8db7c783d
HelloKitty <andrew.blakely@ymail.com>
2020-12-29 02:00:23 -0600

    Added register extensions

---
+++ src/Glader.ASP.RPGCharacter.Server/Extensions/IMvcBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Glader.Essentials;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Glader.ASP.RPGCharacter
+{
+	public static class IMvcBuilderExtensions
+	{
+		/// <summary>
+		/// Registers the general <see cref="CharacterDataController"/> with the MVC
+		/// controllers. See controller documentation for what it does and how it works.
+		/// </summary>
+		/// <param name="builder"></param>
+		/// <returns></returns>
+		public static IMvcBuilder RegisterCharacterDataController(this IMvcBuilder builder)
+		{
+			if(builder == null) throw new ArgumentNullException(nameof(builder));
+
+			return builder.RegisterController<CharacterDataController>();
+		}
+	}
+}
---
+++ src/Glader.ASP.RPGCharacter.Server/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Glader.ASP.RPGCharacter
+{
+	public static class IServiceCollectionExtensions
+	{
+		/// <summary>
+		/// Registers a <see cref="RPGCharacterDatabaseContext"/> and <see cref="IRPGCharacterRepository"/>
+		/// in the provided <see cref="services"/>.
+		/// </summary>
+		/// <param name="services">Service container.</param>
+		/// <param name="optionsAction">The DB context options action.</param>
+		/// <returns></returns>
+		public static IServiceCollection RegisterCharacterDatabase(this IServiceCollection services, Action<DbContextOptionsBuilder> optionsAction)
+		{
+			if (services == null) throw new ArgumentNullException(nameof(services));
+			if (optionsAction == null) throw new ArgumentNullException(nameof(optionsAction));
+
+			//DefaultServiceEndpointRepository : IServiceEndpointRepository
+			services.AddTransient<IRPGCharacterRepository, DefaultRPGCharacterRepository>();
+			services.AddDbContext<RPGCharacterDatabaseContext>(optionsAction);
+
+			//Example:
+			//services.AddDbContext<ServiceDiscoveryDatabaseContext>(builder => { builder.UseMySql("server=127.0.0.1;port=3306;Database=guardians.global;Uid=root;Pwd=test;"); });
+			return services;
+		}
+	}
+}
