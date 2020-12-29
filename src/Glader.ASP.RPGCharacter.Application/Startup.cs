71ec1d3ddb8a02b0cd2717e082e0e1fbe8ee932c
HelloKitty <andrew.blakely@ymail.com>
2020-12-29 02:59:55 -0600

    Added JWT Auth Hacky thingy

--- src/Glader.ASP.RPGCharacter.Application/Startup.cs
+++ src/Glader.ASP.RPGCharacter.Application/Startup.cs
@@ -9,9 +9,12 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Glader.Essentials;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Glader.ASP.RPGCharacter
 {
@@ -40,6 +43,27 @@ namespace Glader.ASP.RPGCharacter
 			});
 
 			services.RegisterGladerASP();
+
+			//TODO: Support real certs.
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options =>
+				{
+					//TODO: This is demo test signing code from: https://github.com/openiddict/openiddict-core/blob/7e1c9dd1307f8127288682459b0b1d82ea804e4f/src/OpenIddict.Server/OpenIddictServerBuilder.cs#L647
+					using var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+					store.Open(OpenFlags.ReadWrite);
+
+					var subject = new X500DistinguishedName("CN=OpenIddict Server Signing Certificate");
+					var certificate = store.Certificates.Find(X509FindType.FindBySubjectDistinguishedName, subject.Name, validOnly: false)
+						.OfType<X509Certificate2>()
+						.SingleOrDefault();
+
+					options.TokenValidationParameters
+						.IssuerSigningKey = new X509SecurityKey(certificate);
+
+					//TODO: This audience stuff is ALL WRONG.
+					options.Audience = "auth-server";
+					options.TokenValidationParameters.ValidIssuer = "https://localhost:5003/";
+				});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
