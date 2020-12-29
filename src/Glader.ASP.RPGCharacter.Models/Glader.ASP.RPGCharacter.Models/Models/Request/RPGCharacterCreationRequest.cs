139afe31a12bcebb0c4a1ed8d5b04b498f610f53
HelloKitty <andrew.blakely@ymail.com>
2020-12-28 23:37:26 -0600

    Added RPGCharacterCreationRequest

---
+++ src/Glader.ASP.RPGCharacter.Models/Glader.ASP.RPGCharacter.Models/Models/Request/RPGCharacterCreationRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Glader.ASP.RPGCharacter
+{
+	/// <summary>
+	/// Represents the minimum required data to create a character.
+	/// </summary>
+	[JsonObject]
+	public sealed class RPGCharacterCreationRequest
+	{
+		/// <summary>
+		/// Requested name of the characters.
+		/// </summary>
+		[JsonProperty]
+		public string Name { get; private set; }
+
+		public RPGCharacterCreationRequest(string name)
+		{
+			Name = name ?? throw new ArgumentNullException(nameof(name));
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		public RPGCharacterCreationRequest()
+		{
+			
+		}
+	}
+}
