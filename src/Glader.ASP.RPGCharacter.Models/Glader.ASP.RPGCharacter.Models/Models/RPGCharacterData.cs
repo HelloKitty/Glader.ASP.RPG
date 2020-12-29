3d5876defb96d90ab9339d5cb5d6aa23ebbba169
HelloKitty <andrew.blakely@ymail.com>
2020-12-28 21:33:55 -0600

    Added basic RPGCharacterData model

---
+++ src/Glader.ASP.RPGCharacter.Models/Glader.ASP.RPGCharacter.Models/Models/RPGCharacterData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Glader.ASP.RPGCharacter
+{
+	/// <summary>
+	/// Contains the full character data for a character.
+	/// </summary>
+	[JsonObject]
+	public sealed class RPGCharacterData
+	{
+		/// <summary>
+		/// The entry data for the character.
+		/// </summary>
+		[JsonProperty]
+		public RPGCharacterEntry Entry { get; private set; }
+
+		/// <summary>
+		/// The character's progress.
+		/// </summary>
+		[JsonProperty]
+		public RPGCharacterProgress Progress { get; private set; }
+
+		public RPGCharacterData(RPGCharacterEntry entry, RPGCharacterProgress progress)
+		{
+			Entry = entry ?? throw new ArgumentNullException(nameof(entry));
+			Progress = progress ?? throw new ArgumentNullException(nameof(progress));
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		public RPGCharacterData()
+		{
+			
+		}
+	}
+}
