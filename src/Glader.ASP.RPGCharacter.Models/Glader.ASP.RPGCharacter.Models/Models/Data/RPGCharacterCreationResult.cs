2053a2199f1f8ee1b40d119e29694868d099e1a6
HelloKitty <andrew.blakely@ymail.com>
2020-12-28 23:50:24 -0600

    Added RPGCharacterCreationResult

---
+++ src/Glader.ASP.RPGCharacter.Models/Glader.ASP.RPGCharacter.Models/Models/Data/RPGCharacterCreationResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Glader.ASP.RPGCharacter
+{
+	[JsonObject]
+	public sealed class RPGCharacterCreationResult
+	{
+		/// <summary>
+		/// The created character id.
+		/// </summary>
+		[JsonProperty]
+		public int Id { get; private set; }
+
+		public RPGCharacterCreationResult(int id)
+		{
+			if (id < 0) throw new ArgumentOutOfRangeException(nameof(id));
+			Id = id;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		public RPGCharacterCreationResult()
+		{
+			
+		}
+	}
+}
