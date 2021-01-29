44ffd9eee5bbbb02fbda5a0e0bbebbe878822aff
HelloKitty <andrew.blakely@ymail.com>
2021-01-28 23:33:54 -0600

    Added IModelDescriptable

---
+++ src/Glader.ASP.RPG.Character.Common/IModelDescriptable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Glader.ASP.RPG
+{
+	/// <summary>
+	/// Contract for types that are self-describing/descriptable.
+	/// </summary>
+	public interface IModelDescriptable
+	{
+		/// <summary>
+		/// The visual human-readable name for the model.
+		/// </summary>
+		string VisualName { get; }
+
+		/// <summary>
+		/// The description of the model.
+		/// </summary>
+		string Description { get; }
+	}
+}
