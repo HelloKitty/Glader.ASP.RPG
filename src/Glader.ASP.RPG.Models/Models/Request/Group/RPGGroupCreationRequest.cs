b0b120f9aa35320290bb462d4da3277e8e39be51
HelloKitty <andrew.blakely@ymail.com>
2021-03-03 04:09:58 -0600

    Added RPGGroupCreationRequest

---
+++ src/Glader.ASP.RPG.Models/Models/Request/Group/RPGGroupCreationRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Glader.ASP.RPG
+{
+	[JsonObject]
+	public sealed class RPGGroupCreationRequest
+	{
+		/// <summary>
+		/// Requested name of the group.
+		/// </summary>
+		[JsonProperty]
+		public string Name { get; private set; }
+
+		/// <summary>
+		/// Represents the requesting creator of the group (leader)
+		/// </summary>
+		[JsonProperty]
+		public int CharacterId { get; private set; }
+
+		public RPGGroupCreationRequest(string name, int characterId)
+		{
+			if (characterId <= 0) throw new ArgumentOutOfRangeException(nameof(characterId));
+
+			Name = name ?? throw new ArgumentNullException(nameof(name));
+			CharacterId = characterId;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		public RPGGroupCreationRequest()
+		{
+			
+		}
+	}
+}
