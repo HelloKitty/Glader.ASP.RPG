d63655d4fe2dea8a60d9c6a533b9289a68d8cb17
HelloKitty <andrew.blakely@ymail.com>
2021-03-11 10:31:50 -0600

    Added IRPGGroupRepository

---
+++ src/Glader.ASP.RPG.Server/Database/Repository/DefaultRPGGroupRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Glader.Essentials;
+using Microsoft.EntityFrameworkCore;
+
+namespace Glader.ASP.RPG
+{
+	/// <summary>
+	/// Default DB/EF Core backed implementation of <see cref="IRPGGroupRepository"/>.
+	/// </summary>
+	public sealed class DefaultRPGGroupRepository : GeneralGenericCrudRepositoryProvider<int, DBRPGGroup>, IRPGGroupRepository
+	{
+		public DefaultRPGGroupRepository(RPGCharacterDatabaseContext context) 
+			: base(context.Groups, context)
+		{
+
+		}
+
+		/// <inheritdoc />
+		public async Task<bool> IsInGroupAsync(int characterId, CancellationToken token = default)
+		{
+			return await Context.Set<DBRPGGroupMember>()
+				.AnyAsync(gm => gm.CharacterId == characterId, token);
+		}
+
+		/// <inheritdoc />
+		public async Task AddMemberAsync(int groupId, int characterId, CancellationToken token = default)
+		{
+			DBRPGGroup group = await RetrieveAsync(groupId, token);
+
+			//TODO: Does this work??
+			group.Members
+				.Add(new DBRPGGroupMember(groupId, characterId));
+
+			await Context.SaveChangesAsync(true, token);
+		}
+
+		/// <inheritdoc />
+		public async Task RemoveMemberAsync(int characterId, CancellationToken token = default)
+		{
+			var member = new DBRPGGroupMember(1, characterId);
+
+			Context.Set<DBRPGGroupMember>().Attach(member);
+			Context.Set<DBRPGGroupMember>().Remove(member);
+
+			await Context.SaveChangesAsync(true, token);
+		}
+
+		/// <inheritdoc />
+		public async Task DeleteAllGroupsAsync()
+		{
+			ModelSet.RemoveRange(ModelSet);
+			await Context.SaveChangesAsync(true);
+		}
+	}
+}
---
+++ src/Glader.ASP.RPG.Server/Database/Repository/IRPGGroupRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Glader.Essentials;
+
+namespace Glader.ASP.RPG
+{
+	/// <summary>
+	/// Simplified type interface for <see cref="IGenericRepositoryCrudable{TKey,TModel}"/>
+	/// for model Type: <see cref="DBRPGGroup"/>.
+	/// </summary>
+	public interface IRPGGroupRepository : IGenericRepositoryCrudable<int, DBRPGGroup>
+	{
+		/// <summary>
+		/// Indicates if the character with the specified id is already in a group.
+		/// </summary>
+		/// <param name="characterId">The character id to check.</param>
+		/// <param name="token">Cancel token.</param>
+		/// <returns>True if the character is in a group.</returns>
+		Task<bool> IsInGroupAsync(int characterId, CancellationToken token = default);
+
+		/// <summary>
+		/// Adds a member to a group with the id <see cref="groupId"/> if it exists.
+		/// </summary>
+		/// <param name="groupId">The group id to add to.</param>
+		/// <param name="characterId">The character to add.</param>
+		/// <param name="token">Cancel token.</param>
+		/// <returns></returns>
+		Task AddMemberAsync(int groupId, int characterId, CancellationToken token = default);
+
+		/// <summary>
+		/// Remove a member to a group with the character id <see cref="characterId"/>.
+		/// </summary>
+		/// <param name="characterId">The character id.</param>
+		/// <param name="token">Cancel token.</param>
+		/// <returns></returns>
+		Task RemoveMemberAsync(int characterId, CancellationToken token = default);
+
+		//Don't make this cancellable, that makes no sense
+		/// <summary>
+		/// Deletes all <see cref="DBRPGGroup"/> from the database.
+		/// </summary>
+		/// <returns></returns>
+		Task DeleteAllGroupsAsync();
+	}
+}
