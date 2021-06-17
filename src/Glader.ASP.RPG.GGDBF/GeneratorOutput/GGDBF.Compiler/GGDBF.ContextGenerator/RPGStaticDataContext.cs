cad7cc1033528fd7b41b26a22ef007af9e2f6787
HelloKitty <andrew.blakely@ymail.com>
2021-06-17 00:58:52 -0500

    Added GGDBF implementation for static game data

---
+++ src/Glader.ASP.RPG.GGDBF/GeneratorOutput/GGDBF.Compiler/GGDBF.ContextGenerator/RPGStaticDataContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Runtime.Serialization;
+using GGDBF;
+using Glader.ASP.RPG;
+
+namespace Glader.ASP.RPG
+{
+    [GeneratedCodeAttribute("GGDBF", "0.0.8.0")]
+    public interface IRPGStaticDataContext<TSkillType, TRaceType, TClassType, TProportionSlotType, TCustomizableSlotType> : IGGDBFContext where TSkillType : System.Enum
+   where TRaceType : System.Enum
+   where TClassType : System.Enum
+   where TProportionSlotType : System.Enum
+   where TCustomizableSlotType : System.Enum
+    {
+        public IReadOnlyDictionary<TSkillType, DBRPGSkill<TSkillType>> Skill { get; init; }
+
+        public IReadOnlyDictionary<TRaceType, DBRPGRace<TRaceType>> Race { get; init; }
+
+        public IReadOnlyDictionary<TClassType, DBRPGClass<TClassType>> @Class { get; init; }
+
+        public IReadOnlyDictionary<TCustomizableSlotType, DBRPGCharacterCustomizableSlotType<TCustomizableSlotType>> CharacterCustomizationSlotType { get; init; }
+
+        public IReadOnlyDictionary<TProportionSlotType, DBRPGCharacterProportionSlotType<TProportionSlotType>> CharacterProportionSlotType { get; init; }
+
+    }
+
+    [GeneratedCodeAttribute("GGDBF", "0.0.8.0")]
+    public partial class RPGStaticDataContext<TSkillType, TRaceType, TClassType, TProportionSlotType, TCustomizableSlotType> : IRPGStaticDataContext<TSkillType, TRaceType, TClassType, TProportionSlotType, TCustomizableSlotType> where TSkillType : System.Enum
+    where TRaceType : System.Enum
+    where TClassType : System.Enum
+    where TProportionSlotType : System.Enum
+    where TCustomizableSlotType : System.Enum
+    {
+        public static RPGStaticDataContext<TSkillType, TRaceType, TClassType, TProportionSlotType, TCustomizableSlotType> Instance { get; private set; }
+
+        public static async Task Initialize(IGGDBFDataSource source)
+        {
+            Instance = new()
+            {
+                Skill = await source.RetrieveTableAsync<TSkillType, DBRPGSkill<TSkillType>>(new NameOverrideTableRetrievalConfig<TSkillType, DBRPGSkill<TSkillType>>("Skill")),
+                Race = await source.RetrieveTableAsync<TRaceType, DBRPGRace<TRaceType>>(new NameOverrideTableRetrievalConfig<TRaceType, DBRPGRace<TRaceType>>("Race")),
+                @Class = await source.RetrieveTableAsync<TClassType, DBRPGClass<TClassType>>(new NameOverrideTableRetrievalConfig<TClassType, DBRPGClass<TClassType>>("Class")),
+                CharacterCustomizationSlotType = await source.RetrieveTableAsync<TCustomizableSlotType, DBRPGCharacterCustomizableSlotType<TCustomizableSlotType>>(new NameOverrideTableRetrievalConfig<TCustomizableSlotType, DBRPGCharacterCustomizableSlotType<TCustomizableSlotType>>("CharacterCustomizationSlotType")),
+                CharacterProportionSlotType = await source.RetrieveTableAsync<TProportionSlotType, DBRPGCharacterProportionSlotType<TProportionSlotType>>(new NameOverrideTableRetrievalConfig<TProportionSlotType, DBRPGCharacterProportionSlotType<TProportionSlotType>>("CharacterProportionSlotType")),
+            };
+        }
+    }
+}
---
+++ src/Glader.ASP.RPG.GGDBF/RPGStaticDataContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using GGDBF;
+
+namespace Glader.ASP.RPG
+{
+	[RequiredDataModel(typeof(DBRPGSkill<>))]
+	[RequiredDataModel(typeof(DBRPGRace<>))]
+	[RequiredDataModel(typeof(DBRPGClass<>))]
+	[RequiredDataModel(typeof(DBRPGCharacterCustomizableSlotType<>))]
+	[RequiredDataModel(typeof(DBRPGCharacterProportionSlotType<>))]
+	public partial class RPGStaticDataContext<TSkillType, TRaceType, TClassType, TProportionSlotType, TCustomizableSlotType> 
+		where TSkillType : Enum 
+		where TRaceType : Enum
+		where TClassType : Enum
+		where TCustomizableSlotType : Enum
+		where TProportionSlotType : Enum
+	{
+		public IReadOnlyDictionary<TSkillType, DBRPGSkill<TSkillType>> Skill { get; init; }
+
+		public IReadOnlyDictionary<TRaceType, DBRPGRace<TRaceType>> Race { get; init; }
+
+		public IReadOnlyDictionary<TClassType, DBRPGClass<TClassType>> @Class { get; init; }
+
+		public IReadOnlyDictionary<TCustomizableSlotType, DBRPGCharacterCustomizableSlotType<TCustomizableSlotType>> CharacterCustomizationSlotType { get; init; }
+
+		public IReadOnlyDictionary<TProportionSlotType, DBRPGCharacterProportionSlotType<TProportionSlotType>> CharacterProportionSlotType { get; init; }
+	}
+}
