9cce80b9e916f0ad4c804b41b8c721281d076a79
HelloKitty <andrew.blakely@ymail.com>
2021-06-18 01:40:34 -0500

    Added CalculateBaseStats extension for DBRPGCharacterDefaultStats

---
+++ src/Glader.ASP.RPG.GGDBF/Collections/BaseStatsCacheDictionary.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.ASP.RPG
+{
+	internal sealed class BaseStatsCacheDictionary<TStatType> : IReadOnlyDictionary<TStatType, RPGStatDefinition<TStatType>>
+		where TStatType : Enum
+	{
+		public static int MaxSize { get; } = Enum.GetValues(typeof(TStatType)).Length;
+
+		public Dictionary<TStatType, RPGStatDefinition<TStatType>> InternalMap { get; }
+
+		public BaseStatsCacheDictionary(Dictionary<TStatType, RPGStatDefinition<TStatType>> internalMap)
+		{
+			InternalMap = internalMap;
+		}
+
+		public BaseStatsCacheDictionary()
+			: this(new Dictionary<TStatType, RPGStatDefinition<TStatType>>(MaxSize))
+		{
+
+		}
+
+		/// <summary>
+		/// Do not call this method externally ever.
+		/// (NOT THREAD-SAFE)
+		/// </summary>
+		/// <param name="statDefinition"></param>
+		public void AddStats(RPGStatDefinition<TStatType> statDefinition)
+		{
+			if (statDefinition == null) throw new ArgumentNullException(nameof(statDefinition));
+
+			if(this.ContainsKey(statDefinition.Id))
+				InternalMap[statDefinition.Id] = new RPGStatDefinition<TStatType>(statDefinition.Id, InternalMap[statDefinition.Id].Value + statDefinition.Value);
+			else
+				InternalMap[statDefinition.Id] = statDefinition;
+		}
+
+		/// <inheritdoc />
+		public IEnumerator<KeyValuePair<TStatType, RPGStatDefinition<TStatType>>> GetEnumerator()
+		{
+			return InternalMap.GetEnumerator();
+		}
+
+		/// <inheritdoc />
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return ((IEnumerable) InternalMap).GetEnumerator();
+		}
+
+		/// <inheritdoc />
+		public int Count => InternalMap.Count;
+
+		/// <inheritdoc />
+		public bool ContainsKey(TStatType key)
+		{
+			return InternalMap.ContainsKey(key);
+		}
+
+		/// <inheritdoc />
+		public bool TryGetValue(TStatType key, out RPGStatDefinition<TStatType> value)
+		{
+			if (ContainsKey(key))
+				return InternalMap.TryGetValue(key, out value);
+
+			//Kinda crappy we have to allocate but it's likely Gen0
+			value = RPGStatDefinition<TStatType>.Empty[key];
+			return true;
+		}
+
+		/// <inheritdoc />
+		public RPGStatDefinition<TStatType> this[TStatType key] => InternalMap[key];
+
+		/// <inheritdoc />
+		public IEnumerable<TStatType> Keys => InternalMap.Keys;
+
+		/// <inheritdoc />
+		public IEnumerable<RPGStatDefinition<TStatType>> Values => InternalMap.Values;
+	}
+}
---
+++ src/Glader.ASP.RPG.GGDBF/Extensions/DBRPGCharacterStatDefaultExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Glader.Essentials;
+
+namespace Glader.ASP.RPG
+{
+	internal static class DBRPGCharacterStatDefaultCache<TStatType, TRaceType, TClassType>
+	{
+		public static IDictionary<DBRPGCharacterStatDefaultKey<TStatType, TRaceType, TClassType>, IReadOnlyDictionary<TStatType, RPGStatDefinition<TStatType>>> BaseStatsCache { get; } = new ConcurrentDictionary<DBRPGCharacterStatDefaultKey<TStatType, TRaceType, TClassType>, IReadOnlyDictionary<TStatType, RPGStatDefinition<TStatType>>>();
+	}
+
+	public static class DBRPGCharacterStatDefaultExtensions
+	{
+		/// <summary>
+		/// Calculates the total base stats a specified <see cref="race"/> and <see cref="@class"/> combo would have at the specified <see cref="level"/>.
+		/// </summary>
+		/// <typeparam name="TStatType">The stat type.</typeparam>
+		/// <typeparam name="TRaceType">The race type.</typeparam>
+		/// <typeparam name="TClassType">The class type.</typeparam>
+		/// <param name="table">DB table.</param>
+		/// <param name="level">The level to calculate the base stats for.</param>
+		/// <param name="race">The race.</param>
+		/// <param name="class">The class.</param>
+		/// <returns>Readonly map of stat types and values.</returns>
+		public static IReadOnlyDictionary<TStatType, RPGStatDefinition<TStatType>> CalculateBaseStats<TStatType, TRaceType, TClassType>(this IReadOnlyDictionary<DBRPGCharacterStatDefaultKey<TStatType, TRaceType, TClassType>, DBRPGCharacterStatDefault<TStatType, TRaceType, TClassType>> table,
+			int level, TRaceType race, TClassType @class)
+			where TRaceType : Enum
+			where TClassType : Enum
+			where TStatType : Enum
+		{
+			var cacheKey = new DBRPGCharacterStatDefaultKey<TStatType, TRaceType, TClassType>(level, race, @class);
+			if (DBRPGCharacterStatDefaultCache<TStatType, TRaceType, TClassType>.BaseStatsCache.ContainsKey(cacheKey))
+				return DBRPGCharacterStatDefaultCache<TStatType, TRaceType, TClassType>.BaseStatsCache[cacheKey];
+
+			BaseStatsCacheDictionary<TStatType> results = new();
+
+			//For every level we sum up the total of the default (base) stats a character would have.
+			table.Where(entry => entry.Value.Level <= level)
+				.SelectMany(entry => entry.Value.Stats)
+				.ForEach(results.AddStats);
+
+			//Add to cache so we don't have to calculate every time.
+			return DBRPGCharacterStatDefaultCache<TStatType, TRaceType, TClassType>.BaseStatsCache[cacheKey] = results;
+		}
+	}
+}
