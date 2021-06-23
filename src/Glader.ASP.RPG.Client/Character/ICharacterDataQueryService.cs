a09b2d7753401273b72faef6cd15079666aa8977
HelloKitty <andrew.blakely@ymail.com>
2021-06-23 03:36:55 -0500

    Created non-generic ICharacterDataQueryService

--- src/Glader.ASP.RPG.Client/Character/ICharacterDataQueryService.cs
+++ src/Glader.ASP.RPG.Client/Character/ICharacterDataQueryService.cs
@@ -12,8 +12,28 @@ namespace Glader.ASP.RPG
 	/// Contract for REST service that provides
 	/// character data query services.
 	/// </summary>
+	public interface ICharacterDataQueryService
+	{
+		/// <summary>
+		/// REST endpoint that gets the character id that the user is authorized for.
+		/// </summary>
+		/// <param name="token">Cancel token.</param>
+		/// <returns>An array of all RPG Character Data.</returns>
+		[RequiresAuthentication]
+		[Get("/api/CharacterData")]
+		Task<int> RetrieveAuthorizedCharacterAsync(CancellationToken token = default);
+
+		//TODO: We should constrain this to a SERVER or ADMIN role
+		[Get("/api/CharacterData/Characters/{id}/account")]
+		Task<ResponseModel<RPGCharacterAccountData, CharacterDataQueryResponseCode>> RetrieveAccountAsync([AliasAs("id")] int characterId, CancellationToken token = default);
+	}
+
+	/// <summary>
+	/// Contract for REST service that provides
+	/// character data query services.
+	/// </summary>
 	[Headers("User-Agent: Glader")]
-	public interface ICharacterDataQueryService<TRaceType, TClassType>
+	public interface ICharacterDataQueryService<TRaceType, TClassType> : ICharacterDataQueryService
 		where TRaceType : Enum
 		where TClassType : Enum
 	{
@@ -36,18 +56,5 @@ namespace Glader.ASP.RPG
 		/// <returns>An array of all RPG Character Data.</returns>
 		[Get("/api/CharacterData/Characters/{id}")]
 		Task<ResponseModel<RPGCharacterData<TRaceType, TClassType>, CharacterDataQueryResponseCode>> RetrieveCharacterDataAsync([AliasAs("id")] int characterId, CancellationToken token = default);
-
-		/// <summary>
-		/// REST endpoint that gets the character id that the user is authorized for.
-		/// </summary>
-		/// <param name="token">Cancel token.</param>
-		/// <returns>An array of all RPG Character Data.</returns>
-		[RequiresAuthentication]
-		[Get("/api/CharacterData")]
-		Task<int> RetrieveAuthorizedCharacterAsync(CancellationToken token = default);
-
-		//TODO: We should constrain this to a SERVER or ADMIN role
-		[Get("/api/CharacterData/Characters/{id}/account")]
-		Task<ResponseModel<RPGCharacterAccountData, CharacterDataQueryResponseCode>> RetrieveAccountAsync([AliasAs("id")] int characterId, CancellationToken token = default);
 	}
 }
