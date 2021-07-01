fd9a2e7c09e74fdaf7f7dc3fa86e002cd00309c2
HelloKitty <andrew.blakely@ymail.com>
2021-07-01 16:05:08 -0500

    Added missing changes for RPG IServiceCollection extensions

--- src/Glader.ASP.RPG.Server/Extensions/IServiceCollectionExtensions.cs
+++ src/Glader.ASP.RPG.Server/Extensions/IServiceCollectionExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Numerics;
 using System.Reflection;
 using System.Text;
+using GGDBF;
 using Glader.Essentials;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Glader.ASP.RPG
 {
 	//<TCustomizableSlotType, TColorStructureType, TProportionSlotType, TProportionStructureType, TRaceType, TClassType, TSkillType, TStatType>
-	public record RPGDatabaseContextOptionsBuilder(Type[] CustomizationTypes, Type[] ProportionTypes, Type RaceType, Type ClassType, Type SkillType, Type StatType)
+	public record RPGOptionsBuilder(Type[] CustomizationTypes, Type[] ProportionTypes, Type RaceType, Type ClassType, Type SkillType, Type StatType)
 	{
 		public enum UninitializedRPGEnum : int
 		{
@@ -19,76 +20,85 @@ namespace Glader.ASP.RPG
 
 		public bool RegisterAsNonGenericDBContext { get; init; } = false;
 
+		public bool RegisterGGDBFSystem { get; init; } = true;
+
 		//<TCustomizableSlotType, TColorStructureType, TProportionSlotType, TProportionStructureType, TRaceType, TClassType, TSkillType, TStatType>
 		internal Type[] BuildTypeParameters()
 		{
 			return new Type[] {CustomizationTypes[0], CustomizationTypes[1], ProportionTypes[0], ProportionTypes[1], RaceType, ClassType, SkillType, StatType};
 		}
 
-		public RPGDatabaseContextOptionsBuilder WithCustomizationType<TCustomizableSlotType, TColorStructureType>()
+		public RPGOptionsBuilder WithCustomizationType<TCustomizableSlotType, TColorStructureType>()
 			where TCustomizableSlotType : Enum
 		{
 			return this with {CustomizationTypes = new[] {typeof(TCustomizableSlotType), typeof(TColorStructureType)}};
 		}
 
-		public RPGDatabaseContextOptionsBuilder WithProportionType<TProportionSlotType, TProportionStructureType>()
+		public RPGOptionsBuilder WithProportionType<TProportionSlotType, TProportionStructureType>()
 			where TProportionSlotType : Enum
 		{
 			return this with { ProportionTypes = new[] { typeof(TProportionSlotType), typeof(TProportionStructureType) } };
 		}
 
-		public RPGDatabaseContextOptionsBuilder WithRaceType<TRaceType>()
+		public RPGOptionsBuilder WithRaceType<TRaceType>()
 			where TRaceType : Enum
 		{
 			return this with { RaceType = typeof(TRaceType) };
 		}
 
-		public RPGDatabaseContextOptionsBuilder WithClassType<TClassType>()
+		public RPGOptionsBuilder WithClassType<TClassType>()
 			where TClassType : Enum
 		{
 			return this with { ClassType = typeof(TClassType) };
 		}
 
-		public RPGDatabaseContextOptionsBuilder WithSkillType<TSkillType>()
+		public RPGOptionsBuilder WithSkillType<TSkillType>()
 			where TSkillType : Enum
 		{
 			return this with { SkillType = typeof(TSkillType) };
 		}
 
-		public RPGDatabaseContextOptionsBuilder WithStatType<TStatType>()
+		public RPGOptionsBuilder WithStatType<TStatType>()
 			where TStatType : Enum
 		{
 			return this with { StatType = typeof(TStatType) };
 		}
 
+
+		internal static RPGOptionsBuilder CreateDefault()
+		{
+			return new RPGOptionsBuilder(
+				ProportionTypes: new Type[2] { typeof(RPGOptionsBuilder.UninitializedRPGEnum), typeof(Vector2<float>) },
+				CustomizationTypes: new Type[2] { typeof(RPGOptionsBuilder.UninitializedRPGEnum), typeof(Vector3<byte>) },
+				RaceType: typeof(RPGOptionsBuilder.UninitializedRPGEnum),
+				ClassType: typeof(RPGOptionsBuilder.UninitializedRPGEnum),
+				SkillType: typeof(RPGOptionsBuilder.UninitializedRPGEnum),
+				StatType: typeof(RPGOptionsBuilder.UninitializedRPGEnum));
+		}
 	}
 
 	public static class IServiceCollectionExtensions
 	{
 		/// <summary>
-		/// Registers a <see cref="RPGCharacterDatabaseContext{TCustomizableSlotType,TColorStructureType,TProportionSlotType,TProportionStructureType,TRaceType,TClassType,TSkillType,TStatType}"/> and <see cref="IRPGCharacterRepository{TRaceType,TClassType}"/>
-		/// in the provided <see cref="services"/>.
+		/// Registers a <see cref="RPGCharacterDatabaseContext{TCustomizableSlotType,TColorStructureType,TProportionSlotType,TProportionStructureType,TRaceType,TClassType,TSkillType,TStatType}"/>
+		/// and <see cref="IRPGCharacterRepository{TRaceType,TClassType}"/>
+		/// and the Glader.ASP.Controllers through <see cref="IMvcBuilderExtensions"/>'s controller methods within
+		/// the provided <see cref="services"/>.
 		/// </summary>
 		/// <param name="services">Service container.</param>
 		/// <param name="optionsAction">The DB context options action.</param>
 		/// <param name="rpgDatabaseOptionsBuilder"></param>
+		/// <param name="mvcBuilder"></param>
 		/// <returns></returns>
-		public static IServiceCollection RegisterCharacterDatabase(this IServiceCollection services, Action<DbContextOptionsBuilder> optionsAction, Func<RPGDatabaseContextOptionsBuilder, RPGDatabaseContextOptionsBuilder> rpgDatabaseOptionsBuilder)
+		public static IServiceCollection RegisterGladerRPGSystem(this IServiceCollection services, Action<DbContextOptionsBuilder> optionsAction, Func<RPGOptionsBuilder, RPGOptionsBuilder> rpgDatabaseOptionsBuilder, IMvcBuilder mvcBuilder)
 		{
 			if (services == null) throw new ArgumentNullException(nameof(services));
 			if (optionsAction == null) throw new ArgumentNullException(nameof(optionsAction));
 			if (rpgDatabaseOptionsBuilder == null) throw new ArgumentNullException(nameof(rpgDatabaseOptionsBuilder));
+			if (mvcBuilder == null) throw new ArgumentNullException(nameof(mvcBuilder));
 
 			//We build the default parameters
-			var rpgOptions = new RPGDatabaseContextOptionsBuilder(
-				ProportionTypes: new Type[2] {typeof(RPGDatabaseContextOptionsBuilder.UninitializedRPGEnum), typeof(Vector2<float>)},
-				CustomizationTypes: new Type[2] {typeof(RPGDatabaseContextOptionsBuilder.UninitializedRPGEnum), typeof(Vector3<byte>)},
-				RaceType: typeof(RPGDatabaseContextOptionsBuilder.UninitializedRPGEnum),
-				ClassType: typeof(RPGDatabaseContextOptionsBuilder.UninitializedRPGEnum),
-				SkillType: typeof(RPGDatabaseContextOptionsBuilder.UninitializedRPGEnum),
-				StatType: typeof(RPGDatabaseContextOptionsBuilder.UninitializedRPGEnum));
-
-			rpgOptions = rpgDatabaseOptionsBuilder(rpgOptions);
+			var rpgOptions = rpgDatabaseOptionsBuilder(RPGOptionsBuilder.CreateDefault());
 			var dbContextType = typeof(RPGCharacterDatabaseContext<,,,,,,,>).MakeGenericType(rpgOptions.BuildTypeParameters());
 
 			//services.AddDbContext<RPGCharacterDatabaseContext<TCustomizableSlotType, TColorStructureType, TProportionSlotType, TProportionStructureType, TRaceType, TClassType, TSkillType, TStatType>>(optionsAction);
@@ -114,6 +124,8 @@ namespace Glader.ASP.RPG
 			//DefaultRPGGroupRepository
 			services.AddTransient<IRPGGroupRepository, DefaultRPGGroupRepository>();
 
+			mvcBuilder.RegisterCharacterDataController(rpgOptions);
+
 			//Example:
 			//services.AddDbContext<ServiceDiscoveryDatabaseContext>(builder => { builder.UseMySql("server=127.0.0.1;port=3306;Database=guardians.global;Uid=root;Pwd=test;"); });
 			return services;
@@ -125,6 +137,35 @@ namespace Glader.ASP.RPG
 			if (services == null) throw new ArgumentNullException(nameof(services));
 
 			services.AddDbContext<TContextType>(optionsAction);
+		}
+
+		public static IServiceCollection RegisterGladerRPGGGDBF<TGGDBFDataSourceType, TGGDBFConverterType>(this IServiceCollection services, Func<RPGOptionsBuilder, RPGOptionsBuilder> rpgDatabaseOptionsBuilder)
+			where TGGDBFDataSourceType : class, IGGDBFDataSource
+			where TGGDBFConverterType : class, IGGDBFDataConverter
+		{
+			if (services == null) throw new ArgumentNullException(nameof(services));
+
+			//RPGStaticDataContext<TestSkillType, TestRaceType, TestClassType, TestProportionSlotType, TestCustomizationSlotType, TestStatType>
+			var rpgOptions = rpgDatabaseOptionsBuilder(RPGOptionsBuilder.CreateDefault());
+
+			//services.RegisterGGDBFContentServices<EntityFrameworkGGDBFDataSource, AutoMapperGGDBFDataConverter, RPGStaticDataContext<TestSkillType, TestRaceType, TestClassType, TestProportionSlotType, TestCustomizationSlotType, TestStatType>>();
+			Type contextType = typeof(RPGStaticDataContext<,,,,,>).MakeGenericType(rpgOptions.SkillType, rpgOptions.RaceType, rpgOptions.ClassType, rpgOptions.ProportionTypes[0], rpgOptions.CustomizationTypes[0], rpgOptions.StatType);
+			typeof(IServiceCollectionExtensions)
+				.GetMethod(nameof(RegisterGGDBFContentServices), BindingFlags.Static | BindingFlags.NonPublic)
+				.MakeGenericMethod(new Type[]{ typeof(TGGDBFDataSourceType), typeof(TGGDBFConverterType), contextType })
+				.Invoke(null, new object[] { services });
+
+			return services;
+		}
+
+		internal static void RegisterGGDBFContentServices<TGGDBFDataSourceType, TGGDBFConverterType, TContextType>(IServiceCollection services)
+			where TGGDBFDataSourceType : class, IGGDBFDataSource
+			where TGGDBFConverterType : class, IGGDBFDataConverter
+			where TContextType : class, IGGDBFContext
+		{
+			if (services == null) throw new ArgumentNullException(nameof(services));
+
+			services.RegisterGGDBFContentServices<TGGDBFDataSourceType, TGGDBFConverterType, TContextType>();
 		}
 	}
 }
