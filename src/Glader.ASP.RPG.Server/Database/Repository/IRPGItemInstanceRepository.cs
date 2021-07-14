b86509b71593434cb19aa05b2bfe379cc2ebd45a
HelloKitty <andrew.blakely@ymail.com>
2021-07-14 01:08:12 -0500

    Created IRPGItemInstanceRepository and default implementation

---
+++ src/Glader.ASP.RPG.Server/Database/Repository/IRPGItemInstanceRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using Microsoft.EntityFrameworkCore;
+
+namespace Glader.ASP.RPG
+{
+	public interface IRPGItemInstanceRepository<TItemClassType, TQualityType, TQualityColorStructureType> : IGenericRepositoryCrudable<int, DBRPGItemInstance<TItemClassType, TQualityType, TQualityColorStructureType>>
+		where TItemClassType : Enum 
+		where TQualityType : Enum
+	{
+
+	}
+
+	public class DefaultRPGItemInstanceRepository<TItemClassType, TQualityType, TQualityColorStructureType> : GeneralGenericCrudRepositoryProvider<int, DBRPGItemInstance<TItemClassType, TQualityType, TQualityColorStructureType>> 
+		where TItemClassType : Enum 
+		where TQualityType : Enum
+	{
+		public DefaultRPGItemInstanceRepository(DbContext context) 
+			: base(context.Set<DBRPGItemInstance<TItemClassType, TQualityType, TQualityColorStructureType>>(), context)
+		{
+
+		}
+	}
+}
