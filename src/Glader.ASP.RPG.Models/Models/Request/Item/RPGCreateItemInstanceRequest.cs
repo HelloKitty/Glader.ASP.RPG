dc80466c9486837c3569947cb924b51a61bc74b4
HelloKitty <andrew.blakely@ymail.com>
2021-07-16 04:47:19 -0500

    Added ItemInstance creation result, request and response

---
+++ src/Glader.ASP.RPG.Models/Models/Request/Item/RPGCreateItemInstanceRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using Newtonsoft.Json;
+
+namespace Glader.ASP.RPG
+{
+	public enum ItemInstanceCreationResult
+	{
+		Success = GladerEssentialsModelConstants.RESPONSE_CODE_SUCCESS_VALUE,
+		GeneralServerError = 2
+	}
+
+	/// <summary>
+	/// Request model for creating an item instance.
+	/// </summary>
+	[JsonObject]
+	public sealed record RPGCreateItemInstanceRequest([property: JsonProperty] int TemplateId);
+
+	/// <summary>
+	/// Response model for creating an item instance.
+	/// Containing the instance id of the newly created item instance.
+	/// </summary>
+	[JsonObject]
+	public sealed record RPGCreateItemInstanceResponse([property: JsonProperty] int InstanceId);
+}
