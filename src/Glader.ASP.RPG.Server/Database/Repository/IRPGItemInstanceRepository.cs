7c5bb145bb9755d1171f3ff72708926c2b4adfe8
HelloKitty <andrew.blakely@ymail.com>
2021-07-16 06:06:45 -0500

    Added specialized TryCreateItemAsync IRPGItemInstanceRepository

--- src/Glader.ASP.RPG.Server/Database/Repository/IRPGItemInstanceRepository.cs
+++ src/Glader.ASP.RPG.Server/Database/Repository/IRPGItemInstanceRepository.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using Glader.Essentials;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,7 +12,13 @@ namespace Glader.ASP.RPG
 		where TItemClassType : Enum 
 		where TQualityType : Enum
 	{
-
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="model">The instance model.</param>
+		/// <param name="token">Cancel token.</param>
+		/// <returns></returns>
+		Task<ItemInstanceCreationResult> TryCreateItemAsync(DBRPGItemInstance<TItemClassType, TQualityType, TQualityColorStructureType> model, CancellationToken token = default);
 	}
 
 	public class DefaultRPGItemInstanceRepository<TItemClassType, TQualityType, TQualityColorStructureType> 
@@ -22,6 +30,32 @@ namespace Glader.ASP.RPG
 			: base(context.Set<DBRPGItemInstance<TItemClassType, TQualityType, TQualityColorStructureType>>(), context)
 		{
 
+		}
+
+		/// <inheritdoc />
+		public async Task<ItemInstanceCreationResult> TryCreateItemAsync(DBRPGItemInstance<TItemClassType, TQualityType, TQualityColorStructureType> model, CancellationToken token = default)
+		{
+			//We can give better error results by checking if the template is missing.
+			if (!await Context
+				.Set<DBRPGItemTemplate<TItemClassType, TQualityType, TQualityColorStructureType>>()
+				.AnyAsync(i => i.Id == model.TemplateId, token))
+			{
+				return ItemInstanceCreationResult.TemplateMissing;
+			}
+
+			try
+			{
+
+				if (await TryCreateAsync(model, token))
+					return ItemInstanceCreationResult.Success;
+				else
+					return ItemInstanceCreationResult.GeneralServerError;
+			}
+			catch (Exception e)
+			{
+				//TODO: Logging
+				return ItemInstanceCreationResult.GeneralServerError;
+			}
 		}
 	}
 }
