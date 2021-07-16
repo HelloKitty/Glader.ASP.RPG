d7ea7e284cabd0e6977c44923edce9ab70e2cbc8
HelloKitty <andrew.blakely@ymail.com>
2021-07-15 21:31:27 -0500

    Added ICharacterInventoryService client

---
+++ src/Glader.ASP.RPG.Client/Character/ICharacterInventoryService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Glader.Essentials;
+using Refit;
+
+namespace Glader.ASP.RPG
+{
+	/// <summary>
+	/// Contract for REST service that provides
+	/// character inventory services.
+	/// </summary>
+	[Headers("User-Agent: Glader")]
+	public interface ICharacterInventoryService
+	{
+		//TODO: Require Role server authorization
+		/// <summary>
+		/// Attempts to add the provided item instance <see cref="instanceId"/> to the character <see cref="characterId"/> inventory.
+		/// The item instance must be unowned.
+		/// </summary>
+		/// <param name="characterId">The character's id.</param>
+		/// <param name="instanceId">The item instance's id.</param>
+		/// <param name="token">The cancel token.</param>
+		/// <returns>True if the item was added to the inventory.</returns>
+		[RequiresAuthentication]
+		[Post("/api/CharacterInventory/{cid}/{iid}/Add")]
+		Task<bool> AddItemAsync([AliasAs("cid")] int characterId, [AliasAs("iid")] int instanceId, CancellationToken token = default);
+
+		//TODO: Require Role server authorization
+		/// <summary>
+		/// Attempts to remove the provided item instance <see cref="instanceId"/> to the character <see cref="characterId"/> inventory.
+		/// The item instance must be owned by the provided Character.
+		/// </summary>
+		/// <param name="characterId">The character's id.</param>
+		/// <param name="instanceId">The item instance's id.</param>
+		/// <param name="token">The cancel token.</param>
+		/// <returns>True if the item was removed from the inventory.</returns>
+		[RequiresAuthentication]
+		[Delete("/api/CharacterInventory/{cid}/{iid}/Remove")]
+		Task<bool> RemoveItemAsync([AliasAs("cid")] int characterId, [AliasAs("iid")] int instanceId, CancellationToken token = default);
+	}
+}
