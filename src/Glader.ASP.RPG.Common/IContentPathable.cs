338053339794c871b613aa49da7fb493bdc5bd9e
HelloKitty <andrew.blakely@ymail.com>
2021-10-18 13:15:21 -0500

    Added IContentPathable

---
+++ src/Glader.ASP.RPG.Common/IContentPathable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.ASP.RPG
+{
+	/// <summary>
+	/// Contract for types that have a path to loadable content.
+	/// </summary>
+	public interface IContentPathable
+	{
+		/// <summary>
+		/// Relative string content load path for the content.
+		/// </summary>
+		string ContentPath { get; }
+	}
+}
